7d06e348dbcfb2f039f2af5651572c6c4687e607
Zehra Demir <zehrademir.gm@gmail.com>
2024-12-15 18:17:04 +0300

    Create models.py

---
+++ app/models.py
@@ -0,0 +1,16 @@
+# app/models.py
+from datetime import datetime
+from app import db
+
+class Task(db.Model):
+    __tablename__ = 'tasks'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text)
+    status = db.Column(db.String(50), default='Pending')
+    assigned_to = db.Column(db.String(255))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Task {self.id} - {self.title}>'
